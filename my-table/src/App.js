56aa508ddc18c9fd8d7520781f98b18d86d3cc74
Aponte Diaz <elifer.l.aponte.diaz@accenture.com>
2019-07-12 16:58:20 -0300

    add table

--- my-table/src/App.js
+++ my-table/src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Table, Button } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props){
+    super(props)
+    this.state = {
+      lista: [
+        {id:1, firtName:"Ana", lastName:"Otto", username:"@jeje"},
+        {id:1, firtName:"pepe", lastName:"Ette", username:"@juju"},
+        {id:1, firtName:"Sandra", lastName:"Uttu", username:"@jiju"}
+      ]
+    }
+  }
+
+  edit(data) {
+    console.table(data)
+  }
+
+  delete(id) {
+    console.log(id)
+  }
+
+  render (){
+    let {lista} = this.state;
+    return (
+    <Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {lista.map((item, i) => {
+            return <tr key= {i}>
+              <th scope="row">{item.id}</th>
+              <td>{item.firtName}</td>
+              <td>{item.lastName}</td>
+              <td>{item.username}</td>
+              <td>
+                <Button color="primary" onClick={this.edit.bind(this,item)}>Edit</Button>{' '}
+                <Button color="secondary" onClick={this.delete.bind(this,item.id)} >Delet</Button>{' '}
+              </td>
+            </tr>
+          })}
+        </tbody>
+    </Table>
+    );
+  }
 }
 
 export default App;
